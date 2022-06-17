cf2ce8688e0b493ec6f0437ec3d481bccef558f9
test <test-user@tests-MacBook-Pro.local>
2022-06-17 11:53:43 -0700

    Made img rotate 90 degrees everytime is clicked until it reaches 100

--- Maui/MyMauiApp/MainPage.xaml.cs
+++ Maui/MyMauiApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	int Rotation = 0;
 
 	public MainPage()
 	{
@@ -12,10 +13,13 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count += 5;
+		Rotation += 90;
 
 		if (count <= 100)
+        {
 			CounterBtn.Text = $"Clicked {count} times";
-
+			Bot.RotateTo(Rotation);
+		}
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
