1f9b743b05cd75187e999955adf8e69494718372
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2022-06-17 10:48:40 -0700

    New PR with the gitignore file [FIXED] (#9)
    * New PR with the gitignore file
    
    * Reducing line/code on MainPage.xaml.cs
    
    Co-authored-by: test <test-user@tests-MacBook-Pro.local>

---
+++ Maui/MyMauiApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MyMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count += 5;
+
+		if (count <= 100)
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
