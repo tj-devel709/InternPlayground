1d496cd2088f4521e810793d8ac361f2c6396977
TJ Lambert <50846373+tj-devel709@users.noreply.github.com>
2022-06-20 12:18:57 -0500

    Adding the base TicTacToe (#15)
    Co-authored-by: TJ Lambert <tjlambert@microsoft.com>

---
+++ TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToe;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	// This is the Event Handler for the TopLeftButton Press
+	void TopLeftBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		var btn = sender as Button;
+		btn.Text = "X";
+		btn.TextColor = Colors.Black;
+		btn.FontSize = 84;
+	}
+}
