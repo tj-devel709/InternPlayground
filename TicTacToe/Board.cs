3ee5495b4cc8069eb40c5263bf32208b1a44ac3c
TJ Lambert <tjlambert@microsoft.com>
2022-06-20 11:51:43 -0500

    Adding the base TicTacToe

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TicTacToe
+{
+	// This is a class.
+	// C# Classes can contain properties, methods, fields, and constructors!
+	public class Board
+	{
+		// This is a constructor
+		public Board ()
+		{
+		}
+
+		// This is a property
+		string SampleProperty { get; set; }
+
+		// This is a field
+		string SampleField = "sample";
+
+		// This is a method
+		public void AddMove()
+		{
+
+		}
+	}
+}
+
