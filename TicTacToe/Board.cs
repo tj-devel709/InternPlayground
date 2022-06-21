99562383a5f75edbe506101a9d0df4cb89c37d12
tj_devel709 <antlambe@microsoft.com>
2022-06-21 12:04:21 -0500

    Game ends but does not reset properly

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -1,25 +1,53 @@
 ﻿using System;
 namespace TicTacToe
 {
-	// This is a class.
-	// C# Classes can contain properties, methods, fields, and constructors!
 	public class Board
 	{
-		// This is a constructor
+		public char[,] BoardArray { get; set; }
+
 		public Board ()
 		{
+			BoardArray = new char[3, 3];
 		}
 
-		// This is a property
-		string SampleProperty { get; set; }
-
-		// This is a field
-		string SampleField = "sample";
-
-		// This is a method
-		public void AddMove()
+		public char AddMove (int row, int col, bool isFirstPlayer)
 		{
+			if (BoardArray[row, col] != default(char))
+				return default(char);
 
+			var marker = isFirstPlayer ? 'X' : 'O';
+
+			BoardArray[row, col] = marker;
+			return marker;
+		}
+
+		public bool CheckWin ()
+        {
+			var isXWin = CheckWin ('X');
+			var isOWin = CheckWin ('O');
+			return isXWin || isOWin;
+        }
+
+		bool CheckWin (char marker)
+        {
+			if ((BoardArray[0, 0] == marker && BoardArray[0, 1] == marker && BoardArray[0, 2] == marker)
+				|| (BoardArray[1, 0] == marker && BoardArray[1, 1] == marker && BoardArray[1, 2] == marker)
+				|| (BoardArray[2, 0] == marker && BoardArray[2, 1] == marker && BoardArray[2, 2] == marker)
+				|| (BoardArray[0, 0] == marker && BoardArray[1, 0] == marker && BoardArray[2, 0] == marker)
+				|| (BoardArray[0, 1] == marker && BoardArray[1, 1] == marker && BoardArray[2, 1] == marker)
+				|| (BoardArray[0, 2] == marker && BoardArray[1, 2] == marker && BoardArray[2, 2] == marker)
+				|| (BoardArray[0, 0] == marker && BoardArray[1, 1] == marker && BoardArray[2, 2] == marker)
+				|| (BoardArray[0, 2] == marker && BoardArray[1, 1] == marker && BoardArray[2, 0] == marker))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public void EraseBoard ()
+        {
+			BoardArray = new char[3, 3];
 		}
 	}
 }
--- TicTacToe/MainPage.xaml.cs
+++ TicTacToe/MainPage.xaml.cs
@@ -3,17 +3,97 @@
 public partial class MainPage : ContentPage
 {
 
+	Board gameBoard;
+	bool isFirstPlayer = true;
+
 	public MainPage()
 	{
 		InitializeComponent();
+		gameBoard = new Board ();
 	}
 
-	// This is the Event Handler for the TopLeftButton Press
-	void TopLeftBtn_Pressed(System.Object sender, System.EventArgs e)
+    void AssignMarker(object sender, int row, int col)
+    {
+        var btn = sender as Button;
+
+        btn.FontSize = 84;
+
+        var marker = gameBoard.AddMove(row, col, isFirstPlayer);
+
+        if (marker != default(char))
+        {
+            btn.Text = marker.ToString();
+            btn.TextColor = isFirstPlayer ? Colors.Black : Colors.Red;
+            isFirstPlayer = !isFirstPlayer;
+        }
+
+        if (gameBoard.CheckWin ())
+            DisplayAlert ("Congrats!", "You won!", "Reset");
+    }
+
+    void TopLeftBtn_Pressed(System.Object sender, System.EventArgs e)
 	{
-		var btn = sender as Button;
-		btn.Text = "X";
-		btn.TextColor = Colors.Black;
-		btn.FontSize = 84;
+        AssignMarker (sender, 0, 0);
 	}
+
+    void TopCenterBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 0, 1);
+    }
+
+    void TopRightBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 0, 2);
+    }
+
+    void CenterLeftBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 1, 0);
+    }
+
+    void CenterCenterBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 1, 1);
+    }
+
+    void CenterRightBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 1, 2);
+    }
+
+    void BottomLeftBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 2, 0);
+    }
+
+    void BottomCenterBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 2, 1);
+    }
+
+    void BottomRightBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        AssignMarker(sender, 2, 2);
+    }
+
+    void ResetBtn_Pressed(System.Object sender, System.EventArgs e)
+    {
+        gameBoard.EraseBoard ();
+
+        //TopLeftBtn.Text = TopCenterBtn.Text = TopRightBtn.Text
+        //    = CenterLeftBtn.Text = CenterCenterBtn.Text = CenterRightBtn.Text
+        //    = BottomLeftBtn.Text = BottomCenterBtn.Text = BottomRightBtn.Text = String.Empty;
+
+        TopLeftBtn.Text = String.Empty;
+        TopCenterBtn.Text = String.Empty;
+        TopRightBtn.Text = String.Empty;
+        CenterLeftBtn.Text = String.Empty;
+        CenterCenterBtn.Text = String.Empty;
+        CenterRightBtn.Text = String.Empty;
+        BottomLeftBtn.Text = String.Empty;
+        BottomCenterBtn.Text = String.Empty;
+        BottomRightBtn.Text = String.Empty;
+
+        isFirstPlayer = true;
+    }
 }
