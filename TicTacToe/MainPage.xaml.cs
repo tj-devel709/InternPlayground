b7c8f63b0c9df37eb518c9875709372bdb5ad8f6
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-06-22 14:01:38 -0700

    Updated TicTacToe code to add button functionality and array component (#19)
    * Updated MyMauiApp to have two buttons (with one saying No Functionality) and changed some of the App's colors.
    
    * Updated code for button color change
    
    * Removed extra line and color
    
    * TicTacToe code with button functionality and array components
    
    * Updated MainPage.xaml.cs code to fix array
    
    Co-authored-by: Admin <admin@Admins-MacBook-Pro.local>

--- TicTacToe/MainPage.xaml.cs
+++ TicTacToe/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 
 public partial class MainPage : ContentPage
 {
+	int count = 0, position = 0;
+	static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7','8' };
 
 	public MainPage()
 	{
@@ -11,9 +13,226 @@ public partial class MainPage : ContentPage
 	// This is the Event Handler for the TopLeftButton Press
 	void TopLeftBtn_Pressed(System.Object sender, System.EventArgs e)
 	{
-		var btn = sender as Button;
-		btn.Text = "X";
-		btn.TextColor = Colors.Black;
-		btn.FontSize = 84;
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 0;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 0;
+			arr[position] = '0';
+			count++;
+		}
 	}
+	void TopCenterBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 1;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 1;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void TopRightBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 2;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 2;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void CenterLeftBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 3;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 3;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void CenterCenterBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 4;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 4;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void CenterRightBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 5;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 5;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void BottomLeftBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 6;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 6;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void BottomCenterBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 7;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 7;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
+	void BottomRightBtn_Pressed(System.Object sender, System.EventArgs e)
+	{
+		if (count % 2 == 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "X";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 8;
+			arr[position] = 'X';
+			count++;
+		}
+
+		else if (count % 2 != 0 && count < 9)
+		{
+			var btn = sender as Button;
+			btn.Text = "0";
+			btn.TextColor = Colors.Black;
+			btn.FontSize = 84;
+			position = 8;
+			arr[position] = '0';
+			count++;
+		}
+	}
+
 }
